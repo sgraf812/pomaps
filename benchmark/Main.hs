c1e1b06a1e07d07a63378ce770e2fb20f266bca4
Sebastian Graf <sgraf1337@gmail.com>
2017-11-13 15:27:02 +0100

    Deleted an ill-typed benchmark

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,9 +1,6 @@
--- You can benchmark your code quickly and effectively with Criterion. See its
--- website for help: <http://www.serpentine.com/criterion/>.
 import           Criterion.Main
-import           Data.POMap.Internal
 
 main :: IO ()
 main = defaultMain
-	[ bench "fromList" (whnf fromList exampleList)
+	[
 	]
